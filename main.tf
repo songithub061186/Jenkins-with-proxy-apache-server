b43551cec6a84c2e3cf5b6b2a5b208eb486b3ff1
songithub061186 <jersonix@gmai.com>
2024-12-14 16:18:32 +0800

    PC folder

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/Users/JERSON POGI/.aws/credentials"]
+  shared_credentials_files = ["/Users/PC/.aws/credentials"]
 }
 
 # Fetch default VPC
